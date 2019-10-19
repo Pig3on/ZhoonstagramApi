34b4db9eaed7ea39c9602d7b494d34bcd867394a
Marin Pavic <marin.pavic@five.agency>
2019-10-19 18:50:07 +0200

    init commit

---
+++ src/main/java/vua/pavic/ZhoonstagramApi/ZhoonstagramApiApplication.java
@@ -0,0 +1,13 @@
+package vua.pavic.ZhoonstagramApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhoonstagramApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhoonstagramApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vua/pavic/ZhoonstagramApi/ZhoonstagramApiApplicationTests.java
@@ -0,0 +1,13 @@
+package vua.pavic.ZhoonstagramApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhoonstagramApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
